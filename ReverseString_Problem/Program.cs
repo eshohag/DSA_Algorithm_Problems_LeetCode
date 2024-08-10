145ad7d1fc735fc621b245bd989917fd30cd539d
eshohag <eshohag@outlook.com>
2024-08-10 19:17:47 +0600

    Reverse String

---
+++ ReverseString_Problem/Program.cs
@@ -0,0 +1,39 @@
+﻿
+namespace ReverseString_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "shohag";
+            var reverse = reverseString(arr: a.ToCharArray());
+            Console.WriteLine(String.Join("", reverse));
+
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Using a Dictionary: Time Complexity: O(n), Space complexity: O(1)
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns>char[] array of indices</returns>
+        private static char[] reverseString(char[] arr)
+        {
+            if (arr.Length < 2)
+            {
+                return arr;
+            }
+            int start = 0, end = arr.Length - 1;
+            char temp;
+
+            while (start < end)
+            {
+                temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                end--;
+                start++;
+            }
+            return arr;
+        }
+    }
+}
