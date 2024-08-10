35484e5439a8b204397919ae9f8043c206dcc125
eshohag <eshohag@outlook.com>
2024-08-10 22:37:58 +0600

    LoopOrCycle_LinkedList-II_Problem

---
+++ LoopOrCycle_LinkedList-II_Problem/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace LoopOrCycle_LinkedList_II_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a hard-coded linked list:
+            // 10 -> 20 -> 30 -> 40 -> 50 -> 60
+            ListNode head = new ListNode(10);
+            head.next = new ListNode(20);
+            head.next.next = new ListNode(30);
+            head.next.next.next = new ListNode(40);
+            head.next.next.next.next = new ListNode(50);
+            head.next.next.next.next.next = new ListNode(60);
+
+            // Creating a loop resulting in the linked list:
+            // 10 -> 20 -> 30 -> 40 -> 10 -> 20 -> 30 ...
+            head.next.next.next.next = head;
+
+            DetectCycle(head);
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Using a Dictionary: Time Complexity: O(n), Space complexity: O(1)
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns>bool array of indices</returns>
+        public static ListNode DetectCycle(ListNode head)
+        {
+            ListNode slow = head, fast = head;
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+
+                if (slow == fast)
+                {
+                    slow = head;
+                    while (slow != fast)
+                    {
+                        slow = slow.next;
+                        fast = fast.next;
+                    }
+                    return slow;
+                }
+            }
+            return null;
+        }
+    }
+}
