393ca3329b2ce5eb5b3387c2d53e7c4e8fccd5db
eshohag <eshohag@outlook.com>
2024-08-10 12:21:20 +0600

    TwoSum_Problem

---
+++ TwoSum_Problem/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TwoSum_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 0, 3, 0 };
+            //int[] array = [1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 7, 1, 1, 1, 1, 1];
+
+            int target = 4;
+
+            int[] normalApproche_Result = TwoSum_Solution.NormalSolution(array, target);
+            Console.WriteLine(string.Join(" ", normalApproche_Result));
+
+            int[] optimizedSolution = TwoSum_Solution.OptimizedSolution(array, target);
+            Console.WriteLine(string.Join(" ", optimizedSolution));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TwoSum_Problem/TwoSum_Solution.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum_Problem
+{
+    public class TwoSum_Solution
+    { /// <summary>
+      /// Brute force: Time Complexity: O(n^2), Space complexity: O(1)
+      /// </summary>
+      /// <param name="array"></param>
+      /// <param name="target"></param>
+      /// <returns>integer array of indices</returns>
+        public static int[] NormalSolution(int[] array, int target)
+        {
+            //Declarations
+            int length = array.Length;
+
+            //Validations
+            if (array == null || length < 2)
+            {
+                return Array.Empty<int>();
+            }
+
+            //Logic
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = i + 1; j < length; j++)
+                {
+                    if (array[i] + array[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return Array.Empty<int>();
+        }
+
+        /// <summary>
+        /// Using a Dictionary: Time Complexity: O(n), Space complexity: O(n)
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="target"></param>
+        /// <returns>integer array of indices</returns>
+        public static int[] OptimizedSolution(int[] array, int target)
+        {
+            //Declarations
+            int length = array.Length;
+
+            //Key, Value
+            Dictionary<int, int> result = new Dictionary<int, int>();
+
+            //Validations
+            if (array == null || length < 2)
+            {
+                return Array.Empty<int>();
+            }
+
+            //Logic
+            //x+y=target
+            //firstNumber+secondNumber=target
+            //secondNumber=target-firstNumber;
+            for (int i = 0; i < length; i++)
+            {
+                int firstNumber = array[i];
+                int secondNumber = target - firstNumber;
+                if (result.ContainsKey(secondNumber))
+                {
+                    return new[] { result[secondNumber], i };
+                }
+                result[firstNumber] = i;
+            }
+
+            return Array.Empty<int>(); ;
+        }
+    }
+}
