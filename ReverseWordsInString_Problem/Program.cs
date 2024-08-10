794ab6276acf2416761df65cd1a57e72e861abcd
eshohag <eshohag@outlook.com>
2024-08-10 20:53:14 +0600

    ReverseWordsInString_Problem

---
+++ ReverseWordsInString_Problem/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+namespace ReverseWordsInString_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var s = "the sky is blue";
+            var s = "a good   example";
+            Console.WriteLine(ReverseWords(s));
+            Console.ReadKey();
+        }
+        public static string ReverseWords(string s)
+        {
+            var options = StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries;
+            string[] array = s.Split(' ', options);
+            int start = 0, end = array.Length - 1;
+            string temp = String.Empty;
+            while (start < end)
+            {
+                temp = array[start];
+                array[start] = array[end];
+                array[end] = temp;
+
+                end--;
+                start++;
+            }
+            return String.Join(" ", array);
+        }
+    }
+}
---
+++ ReverseWords_Problem/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+namespace ReverseWords_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Let's Write an algorithm to reverse words";
+            string result = ReverseWords(s);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        private static string ReverseWords(string s)
+        {
+            var sBuilder = new System.Text.StringBuilder();
+
+            foreach (string word in s.Split(' '))
+            {
+                sBuilder.Append(Reverse(word)).Append(" ");
+            }
+            return sBuilder.ToString();
+        }
+
+        private static string Reverse(string word)
+        {
+            char[] ch = word.ToCharArray();
+            int start = 0, end = ch.Length - 1;
+            char temp;
+            while (start < end)
+            {
+                temp = ch[start];
+                ch[start] = ch[end];
+                ch[end] = temp;
+
+                end--;
+                start++;
+            }
+            return new string(ch);
+        }
+    }
+}
