e3a000a5983c191135a94fa145a75ecf08e8b3af
eshohag <eshohag@outlook.com>
2024-08-13 23:12:06 +0600

    StackUsingQueues

---
+++ StackUsingQueues_Problem/MyStack_Solution.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackUsingQueues_Problem
+{
+    public class MyStack_Solution
+    {
+        Queue<int> queue;
+        /** Initialize your data structure here. */
+        public MyStack_Solution()
+        {
+            this.queue = new Queue<int>();
+        }
+
+        /** Push element x onto stack. */
+        public void Push(int x)
+        {
+            this.queue.Enqueue(x);
+            int n = queue.Count;
+            while (n > 1)
+            {
+                queue.Enqueue(queue.Dequeue());
+                n--;
+            }
+        }
+
+        /** Removes the element on top of the stack and returns that element. */
+        public int Pop()
+        {
+            return queue.Dequeue();
+        }
+
+        /** Get the top element. */
+        public int Top()
+        {
+            return queue.Peek();
+        }
+
+        /** Returns whether the stack is empty. */
+        public bool Empty()
+        {
+            if (queue.Count == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ StackUsingQueues_Problem/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace StackUsingQueues_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack_Solution obj = new MyStack_Solution();
+            obj.Push(1);
+            int param_2 = obj.Pop();
+            int param_3 = obj.Top();
+            bool param_4 = obj.Empty();
+
+            Console.ReadKey();
+        }
+    }
+}
