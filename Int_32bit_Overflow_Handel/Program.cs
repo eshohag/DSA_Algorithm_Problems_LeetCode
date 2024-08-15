a6a7aa793c7dcdd8ab503b561c523b5e65c9e6c7
eshohag <eshohag@outlook.com>
2024-08-15 22:23:50 +0600

    Int_32bit_Overflow_Handel

---
+++ Int_32bit_Overflow_Handel/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Int_32bit_Overflow_Handel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                unchecked
+                {
+                    int max = int.MaxValue;
+                    Console.WriteLine($"int.MaxValue {max:N0}");
+                    int sum = max++;
+                    Console.WriteLine($"Total Sum {sum:N0}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
