2d5bd0c80c303f742ad82f944d416e2e84aa1ddf
eshohag <eshohag@outlook.com>
2024-08-16 20:59:17 +0600

    Queue_using_Array_or_Circular_Queue

---
+++ Queue_using_Array_or_Circular_Queue/MyCircularQueue.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_using_Array_or_Circular_Queue
+{
+    public class MyCircularQueue
+    {
+        private int[] buf;
+        private int start;
+        private int end;
+
+        public MyCircularQueue(int k)
+        {
+            start = -1;
+            end = -1;
+            buf = new int[k];
+        }
+
+        public bool EnQueue(int value)
+        {
+            if (IsFull()) return false;
+            if (start == -1)
+            {
+                start = 0;
+            }
+            ++end;
+            if (end == buf.Length) end = 0;
+            buf[end] = value;
+            return true;
+        }
+
+        public bool DeQueue()
+        {
+            if (IsEmpty()) return false;
+            if (start == end)
+            {
+                start = -1;
+                end = -1;
+            }
+            else
+            {
+                ++start;
+                if (start == buf.Length) start = 0;
+            }
+            return true;
+        }
+
+        public int Front()
+        {
+            if (IsEmpty()) return -1;
+            return buf[start];
+        }
+
+        public int Rear()
+        {
+            if (IsEmpty()) return -1;
+            return buf[end];
+        }
+
+        public bool IsEmpty()
+        {
+            return start == -1;
+        }
+
+        public bool IsFull()
+        {
+            return start == ((end + 1) % buf.Length);
+        }
+    }
+}
--- Queue_using_Array_or_Circular_Queue/Program.cs
+++ Queue_using_Array_or_Circular_Queue/Program.cs
@@ -4,6 +4,13 @@
     {
         static void Main(string[] args)
         {
+            MyCircularQueue obj = new MyCircularQueue(10);
+            bool param_1 = obj.EnQueue(1);
+            bool param_2 = obj.DeQueue();
+            int param_3 = obj.Front();
+            int param_4 = obj.Rear();
+            bool param_5 = obj.IsEmpty();
+            bool param_6 = obj.IsFull();
             Console.ReadKey();
         }
 
