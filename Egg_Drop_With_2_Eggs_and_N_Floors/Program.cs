43714c636e11d1223952d854ea47d4cdb078e0d2
eshohag <eshohag@outlook.com>
2024-08-16 19:51:28 +0600

    Egg_Drop_With_2_Eggs_and_N_Floors

---
+++ Egg_Drop_With_2_Eggs_and_N_Floors/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Egg_Drop_With_2_Eggs_and_N_Floors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = TwoEggDrop(10);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+        public static int TwoEggDrop(int n)
+        {
+            int moves = 0;
+            int floorsToCheck = 0;
+
+            while (floorsToCheck < n)
+            {
+                moves++;
+                floorsToCheck += moves;
+            }
+            return moves;
+        }
+    }
+}
