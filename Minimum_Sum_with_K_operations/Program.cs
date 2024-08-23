fdfc1704705a84e54d62490e3907351c99ee1304
eshohag <eshohag@outlook.com>
2024-08-23 17:31:52 +0600

    Minimum_Sum_with_K_operations

---
+++ Minimum_Sum_with_K_operations/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Minimum_Sum_with_K_operations
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = [ 10, 20, 7 ];
+            int k = 4;
+            Array.Sort(arr);
+            while (k > 0)
+            {
+                var Ceiling = Math.Ceiling((decimal)arr[arr.Length - 1] / (decimal)2);
+                arr[arr.Length - 1] = (int)Ceiling;
+                Array.Sort(arr);
+                k--;
+            }
+
+            Console.WriteLine("Array is "+String.Join(" ", arr)+ ", Total Sum is " + arr.Sum());
+
+            Console.ReadKey();
+        }
+    }
+}
